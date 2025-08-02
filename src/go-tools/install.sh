5a1b77207508d91da69f317e55f043de17cd4a4c
Marcel Kocks <marcel.kocks@hotmail.de>
2025-08-02 20:56:00 +0000

    feat: install multiple features

--- src/go-tools/install.sh
+++ src/go-tools/install.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 set -e
 
-go install ${TOOL}
+# Loop through TOOL0 to TOOL9
+for i in $(seq 0 9); do
+    tool_var="TOOL${i}"
+    tool_value=$(eval echo \$$tool_var)
+    if [ -n "${tool_value}" ]; then
+        go install ${tool_value}
+    fi
+done
